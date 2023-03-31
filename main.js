9042f3eb7cf905de4fd0730095c34b10e6e47c05
HappyUnknown <39666812+HappyUnknown@users.noreply.github.com>
2023-03-31 15:17:33 +0300

    Switch to the next task will re-generate task elements, and clear the previous

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 function hideAll()
 {
    var parentID = document.getElementById('mainDiv');
-   var elements = parentID.getElementsByClassName('taskTable');
-   for(let i=0;i<elements.length;i++)
-      elements[i].style.display = 'none'
+   var panels = parentID.getElementsByClassName('taskTable');
+   for(let i=0;i<panels.length;i++)
+   {
+      panels[i].style.display = 'none'
+      panels[i].innerHTML = "";
+   }
+}
+function showTaskById(id)
+{
+   hideAll();
+   document.getElementById(`tblTask${id}`).style.display = 'block';
 }
 function onloadfunc()
 {
-   task1();
+   processTask1();
+   showTaskById(1);
 }
 function createButtonTask1(text)
 {
@@ -22,8 +31,20 @@ function createButtonTask1(text)
       let breakline = document.createElement("br");
       document.getElementById("tblTask1").appendChild(breakline);
 }
+function createButtonTask2(text)
+{
+      let btn = document.createElement("button");
+      btn.innerHTML = text;
+      btn.className="buttonTask2"
+      btn.addEventListener("click", function () {
+      alert("Button is clicked");
+      });
+      document.getElementById("tblTask2").appendChild(btn);
 
-function task1()
+      let breakline = document.createElement("br");
+      document.getElementById("tblTask2").appendChild(breakline);
+}
+function processTask1()
 {
    let size = 10;
    let arr = new Array();
@@ -51,5 +72,33 @@ function task1()
    {
       alert(ex);
    }
+}
+function processTask2()
+{
+   let size = 10;
+   let arr = new Array();
+   try
+   {
+      for(let i=0;i<size;i++)
+      {
+         let num = Math.random()*99+1;
+         arr[i]=parseInt(num);
+      }
+   }
+   catch(ex)
+   {
+      alert(ex);
+   }
 
+   try
+   {
+      for(let i=0;i<size;i++)
+      {
+         createButtonTask2(`[${i}]: ${arr[i]}`);
+      }   
+   }
+   catch(ex)
+   {
+      alert(ex);
+   }
 }
