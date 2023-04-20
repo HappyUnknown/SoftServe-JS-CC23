2ce30a26f139b241266fced19a8cf30f147f1b6e
HappyUnknown <39666812+HappyUnknown@users.noreply.github.com>
2023-04-20 10:47:42 +0300

    Search modelname with alert result (Need filling algo)

--- main.js
+++ main.js
@@ -7,24 +7,50 @@ function addRow()
         Model: document.getElementById("tbModelname").value,
         Year: document.getElementById("ypYear").value,
         Price: document.getElementById("tbPrice").value,
-        Color: document.getElementById("cpDesign").value,
+        Color: document.getElementById("cpPaint").value,
         Status: document.getElementById("tbStatus").value 
     }
     cars.push(row);
 
     document.getElementById("tblCars").innerHTML += 
-    `<tr>
+    `
+    <tr>
         <th scope="row">${row.ID}</th>
         <td>${row.Model}</td>
         <td>${row.Year}</td>
         <td>${row.Price}</td>
         <td>${row.Color}</td>
         <td>${row.Status}</td>
+    </tr>
     `
 }
+function getElementByModel()
+{
+    for(let i=0;i<cars.length;i++)
+    {
+        if(cars[i].Model == document.getElementById("tbModelname").value)
+        {
+            // document.getElementById("tbModelname").value = cars[i].Model;
+            // document.getElementById("ypYear").value = cars[i].Year;
+            // document.getElementById("tbPrice").value = cars[i].Price;
+            // document.getElementById("cpPaint").value = cars[i].Color;
+            // document.getElementById("tbStatus").value = cars[i].Status;
+            alert(`WITH PAINT OF ${cars[i].Color}`);
+            return;
+        }
+    }
+    alert("No such modelname");
+}
 
-const addBtn = document.getElementById("btnAddItem");
-addBtn.onclick = (event) =>
+const searchBTN = document.getElementById("btnModelSearch");
+searchBTN.onclick = (event) =>
+{
+    event.preventDefault();
+    getElementByModel();
+}
+
+const addBTN = document.getElementById("btnAddItem");
+addBTN.onclick = (event) =>
 {
     event.preventDefault();
     addRow();
